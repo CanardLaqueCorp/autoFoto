15f981dddeeb100dbda1df10c44343bcf80e890d
NicoutG <123987124+NicoutG@users.noreply.github.com>
2023-09-12 15:07:50 +0200

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -19,7 +19,22 @@ def crea():
         brand_model = car["brand"] + " " + car["model"]
         print(brand_model)
         models.append(brand_model)
-    return 
+    return models
+
+def load(past):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
+    url = ("https://www.google.com/search?q={s}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568")
+    driver.get(url.format(s='presentation+{}'.format(past.replace(' ','+'))))
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+    time.sleep(1)
+    imgResults = driver.find_elements(By.XPATH,"//img[contains(@class,'Q4LuWd')]")
+    src=imgResults[0].get_attribute('src')
+    urllib.request.urlretrieve(str(src),"data/{}.jpg".format(past.replace(' ','_')))
+    return
 
 def findImages():
     print("we generate the array of models")
@@ -27,5 +42,6 @@ def findImages():
     modelToFindPhotos=crea()
     print("we search for images of the models")
     print(modelToFindPhotos)
-
+    for model in  modelToFindPhotos:
+        load(model)
     return
