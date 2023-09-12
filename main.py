e34baa6c2c46adeea983c9dcbcf30fa2f3a1ff09
Claquettes <math@claq.fr>
2023-09-12 08:38:08 +0200

    verif try ==> except for opening the data files

--- main.py
+++ main.py
@@ -4,7 +4,18 @@ def main():
     loadData = input("Would you like to load data from a file? (it should be placed in the data folder) (y/n): ")
     if loadData == "y":
         print("Loading data...")
-
+        #we try to open data/data.json
+        try:
+            dataFile = open("data/data.json", "r")
+            print("Data loaded!")
+            print("Starting program...")
+            #we close the file
+            dataFile.close()
+        #if we can't open the file, we print an error message
+        except:
+            print("Error: couldn't load data.")
+            print("Make sure that the data folder exists and that it contains a file named data.json")
     else:
         print("No data loaded, end of program.")
 main()
+
