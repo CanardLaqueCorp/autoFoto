1c925cbea113e27ff62e160b4c74c9acdf0f5491
Claquettes <math@claq.fr>
2023-09-12 18:39:35 +0200

    fully working code that allow to not start again every time merci maxime tu es trop fort merci d2d et en plus 135 super images même si la buick est certainement pas bonne

--- src/functions.py
+++ src/functions.py
@@ -4,7 +4,23 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import urllib
+import os
 import time
+
+
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome()
+    
+#we open google.com
+driver.get("https://www.google.com")   
+url = ("https://www.google.com/search?q={s}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568")
+
 
 def crea():
     dataPath = "data/data.json"
@@ -20,29 +36,35 @@ def crea():
     else:
         print("Error loading data")
         return
+    
+    idAlreadyUsed = []
+    #we go through every file in the img folder, and we get the id (the name of the file without the extension)
+    for filename in os.listdir("img"):
+        idAlreadyUsed.append(filename.split(".")[0])
+    print("idAlreadyUsed: " + str(idAlreadyUsed))
+    
+
+
 
     for car in data["result"]:
         brand_model = car["brand"] + " " + car["model"]
-        print(brand_model)
-        models.append([brand_model, car["id"]])
+        if str(car["id"]) not in idAlreadyUsed:
+            models.append([brand_model, car["id"]])
+        
     return models
 
 def load(past, id):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-
-    driver = webdriver.Chrome()
-
-    url = ("https://www.google.com/search?q={s}&tbm=isch&tbs=sur%3Afc&hl=en&ved=0CAIQpwVqFwoTCKCa1c6s4-oCFQAAAAAdAAAAABAC&biw=1251&bih=568")
-    driver.get(url.format(s='image{}'.format(past.replace(' ','+'))))
+    
+    time.sleep(1)
+    driver.get(url.format(s='photo{}'.format(past.replace(' ','+'))))
     driver.execute_script("window.scrollTo(0,document.body.scrollHeight);")
     time.sleep(3)
     imgResults = driver.find_elements(By.XPATH,"//img[contains(@class,'Q4LuWd')]")
     src=imgResults[0].get_attribute('src')
     urllib.request.urlretrieve(str(src),"img/{}.jpg".format(id))
     return
+
+
 
 def findImages():
     print("we generate the array of models")
